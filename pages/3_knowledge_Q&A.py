d0185de68f583a74fe38782fcbefbb509945084d
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2024-06-24 22:45:20 +0200

    Update 3_knowledge_Q&A.py

--- pages/3_knowledge_Q&A.py
+++ pages/3_knowledge_Q&A.py
@@ -102,7 +102,7 @@ if uploaded_file is not None:
         db_FAISS = FAISS.from_documents(pdf_chunks, embeddings)
         retriever = db_FAISS.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.6})
         qa = RetrievalQA.from_chain_type(llm=chat, chain_type="stuff", retriever=retriever, return_source_documents=True)
-    else:
+    #else:
         #chat = ChatLlamaCpp(temperature=0, model_name='llama3-70b', api_key=api_key)
         #embeddings = LlamaCppEmbeddings(api_key=api_key)
         #db_FAISS = FAISS.from_documents(pdf_chunks, embeddings)
