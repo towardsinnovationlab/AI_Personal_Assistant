7590b9c61c632917924d36156be6bf3fef48a584
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2024-06-26 06:39:50 +0200

    Add files via upload

--- pages/2_chatbot.py
+++ pages/2_chatbot.py
@@ -16,7 +16,7 @@ with st.sidebar:
 
     # Reset button
     if st.button('Reset Conversation'):
-        st.session_state["messages"] = []
+        st.session_state.chatbot["messages"] = []
         st.info("Please change your API_KEY if you change model.")
     
     st.write("check out this [article](https://medium.com/@c.giancaterino/build-your-personal-ai-assistant-with-streamlit-and-llms-2f95c9b00e0b)")
@@ -28,11 +28,11 @@ st.caption("🚀 Your Personal AI Assistant powered by Streamlit and LLMs")
 
 
 # Initialize messages if not present in session state
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+if "messages" not in st.session_state.chatbot:
+    st.session_state.chatbot["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 # Display messages
-for msg in st.session_state.messages:
+for msg in st.session_state.chatbot.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 
@@ -43,22 +43,22 @@ if prompt := st.chat_input():
         st.stop()
 
     # Append user message to session state
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.chatbot.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
     # Client initialization based on selected model
     if option == 'Mixtral 8x7B':
         client = MistralClient(api_key=api_key)
-        response = client.chat(model="open-mixtral-8x7b", messages=st.session_state.messages)
+        response = client.chat(model="open-mixtral-8x7b", messages=st.session_state.chatbot.messages)
     elif option == 'GPT-4o':
         client = OpenAI(api_key=api_key)
-        response = client.chat.completions.create(model="gpt-4o", messages=st.session_state.messages)    
+        response = client.chat.completions.create(model="gpt-4o", messages=st.session_state.chatbot.messages)    
     elif option == 'GPT-3.5-turbo':
         client = OpenAI(api_key=api_key)
-        response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+        response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.chatbot.messages)
     elif option == 'Llama-3-70B':
         client = OpenAI(api_key=api_key,base_url="https://api.llama-api.com")
-        response = client.chat.completions.create(model="llama3-70b", messages=st.session_state.messages)
+        response = client.chat.completions.create(model="llama3-70b", messages=st.session_state.chatbot.messages)
     else:
         st.error("Selected model is not supported.")
         st.stop()
@@ -66,5 +66,5 @@ if prompt := st.chat_input():
 
     # Process response and update session state
     msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.session_state.chatbot.chatbot.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
