cf1b2ce6d9e0ee22489165c1d75fcfbf46ec8ac9
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2024-06-27 07:18:56 +0200

    Add files via upload

---
+++ pages/1_app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+
+# Title and caption
+st.title("💬 AI Chatbot development")
+st.caption("🚀 Your Personal AI Assistant helping in your activities")
+
+
+
+
+
+st.markdown("""
+        **👈 Select a page from the dropdown on the left** to see some applications of the app!""")
+
+
+
+st.image("./images/magicstudio-art_ai_1.jpg", width=500)
+
+
+
