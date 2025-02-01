8c18baf5a41f7fda03d70a7c97135bb99023c53f
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2025-02-01 07:29:07 +0100

    Update 2_chatbot.py

--- pages/2_chatbot.py
+++ pages/2_chatbot.py
@@ -95,4 +95,4 @@ if prompt := st.chat_input():
     # Process response and update session state
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+    st.chat_message("assistant").markdown(msg)
